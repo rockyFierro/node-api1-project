f27001f537f65d3da350d72081d7ef5291acecbc
rockyFierro <finnslighthouse@gmail.com>
2021-07-06 23:31:56 -0700

    added express, made a simple request

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+
+const server = express();
+
+server.get('/',
+(require, respond)=>{
+  respond.send('hello there, express welcomes you.');
+});
+
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
