07018df927758b111f54625e815fe4a23075b099
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-09 12:11:08 -0700

    added REST client tool; dev

--- api/server.js
+++ api/server.js
@@ -3,22 +3,34 @@ const express = require('express');
 const server = express();
 const User = require('./users/model')
 
-server.post('/api/users', async (req,res,newUser)=>{
-    const  users = await User.insert(newUser);
-    res.status(201).json(users);
+server.use(express.json())
+
+server.post('/api/users', async (request, respond) => {
+  const data = request.body;
+  const newData = await User.insert(data);
+  respond.status(201).json(newData);
 })
 
 server.get('/',
-  (require, respond) => {
+  (request, respond) => {
     respond.send('hello there, express welcomes you.');
   });
 
-server.get('/api/users', async (require, respond) => {
-    const users = await User.find();
-    respond.status(200).json(users);
-  });
+server.get('/api/users', async (request, respond) => {
+  const users = await User.find();
+  respond.status(200).json(users);
+});
 
+server.get('/api/users/:id', async (request, response) => {
+  const { id } = request.params.id;
+  const user = await User.findById(id);
+  response.status(200).json(user);
+});
 
-
+server.delete('/api/users/:id', async (request, response)=>{
+  const { id } = request.params.id;
+  const data = await User.remove(id);
+  response.status(204).json(data);
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
