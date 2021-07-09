d83ff1f341d0dae533056c5cd5697c2fa7a6d0dd
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-08 20:07:58 -0700

    added post method

--- api/server.js
+++ api/server.js
@@ -1,28 +1,23 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const users = require('./users/model');
 const server = express();
+const User = require('./users/model')
 
+server.post('/api/users', async (req,res,newUser)=>{
+    const  users = await User.insert(newUser);
+    res.status(201).json(users);
+})
 
 server.get('/',
   (require, respond) => {
     respond.send('hello there, express welcomes you.');
   });
 
-server.get('/api/users',
-  (require, respond) => {
-    const users = [
-      {
-        id: 1,
-        name: 'Guy Montag'
-      },
-      {
-        id: 2,
-        name: 'Dorian Gray'
-      }
-    ]
+server.get('/api/users', async (require, respond) => {
+    const users = await User.find();
     respond.status(200).json(users);
   });
+
 
 
 
